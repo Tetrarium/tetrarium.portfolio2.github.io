a186362b8142eba76663be53b16197f2ff7114b4
Tetrarium <95538514+Tetrarium@users.noreply.github.com>
2022-03-19 19:25:24 +0700

    Update accordeon.js

--- js/accordeon.js
+++ js/accordeon.js
@@ -2,11 +2,13 @@ const accordeon = document.querySelector('.feature-list');
 const accordeonButtons = accordeon.querySelectorAll('.feature__link');
 
 accordeonButtons.forEach((btn) => {
-	btn.addEventListener('click', () => {
-		accordeonButtons.forEach((button) => {
-			button.classList.remove('feature__link_active');
-			button.nextElementSibling.classList.add('hidden');
-		})
+	btn.addEventListener('click', (event) => {
+		if (!event.target.closest('.feature__link').classList.contains('feature__link_active')) {
+			accordeonButtons.forEach((button) => {
+				button.classList.remove('feature__link_active');
+				button.nextElementSibling.classList.add('hidden');
+			})
+		}
 
 		btn.classList.toggle('feature__link_active');
 		btn.nextElementSibling.classList.toggle('hidden');
